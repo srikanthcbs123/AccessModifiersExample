744e715b7aeddaec1891e1d3a3ac9db2eb3f8992
srikanthcbs123 <srikanthcbs123@gmail.com>
2024-10-09 06:53:38 +0530

    Userstory:345666 employee class added

---
+++ AccessModifiersExample/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiersExample
+{//uncomment and check and explain this negative scenario.
+    //private class Employee
+    //{
+    //}
+}
